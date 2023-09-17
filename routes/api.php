28bf87042eecad09fa213fc44583bc57e8427b32
Chandar887 <chandarbhan887@gmail.com>
2023-09-17 23:55:44 +0530

    login register api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(ApiController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/register', 'register');
+});
+
+
+// Auth middleware
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/dashboard', function (Request $request) {
+        return $request->user();
+    });
 });
