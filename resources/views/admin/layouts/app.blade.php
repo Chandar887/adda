bf1b1900056101ad0f88683ccf186bd83260f4ad
Chandar887 <chandarbhan887@gmail.com>
2023-09-17 14:29:10 +0530

    adda project

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,9 @@
+@include('admin.includes.header')
+
+<!-- Content wrapper -->
+<div class="content-wrapper">
+    @yield('section')
+</div>
+<!-- Content wrapper -->
+
+@include('admin.includes.footer')
